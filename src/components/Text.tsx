6171198f8d1d92a89b651bf3d657ad53196c0568
Mahdi Rezayi <m.rezayi853mh77@gmail.com>
2023-03-09 22:50:42 +0330

    Getting started

---
+++ src/components/Text.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const Text = ({
+  as,
+  children,
+}: {
+  as?: any
+  children: React.ReactNode
+}) => {
+  const Component = as || 'span'
+
+  return <Component>{children}</Component>
+}
